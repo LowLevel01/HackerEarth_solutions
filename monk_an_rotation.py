032924654ae9dfc163fa2927e2e5a57b85f58233
Zakariaa Hamid <88563786+LowLevel01@users.noreply.github.com>
2023-02-11 23:03:14 +0100

    Create monk_an_rotation.py

---
+++ monk_an_rotation.py
@@ -0,0 +1,6 @@
+t  = int(input())
+for _ in range(testCase):
+    n, k = map(int,input().split())
+    l = list(map(int,input().split()))
+    m = k % n          #if k > n then a rotation by k is equivalent to a rotation by k%n
+    print(*(l[n-m:]+l[:n-m]))        # a rotation by m is equivalent to dividing the list l into two lists l[n-m:] and l[:n-m] and exchanging them
